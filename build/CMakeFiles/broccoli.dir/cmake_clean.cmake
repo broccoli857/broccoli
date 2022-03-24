b3121f90174dccfc70ee8a6725484a8ce3100014
chengyanfu <cheng@localhost.localdomain>
2022-03-24 17:46:54 +0800

    exchange to CentOS7

---
+++ build/CMakeFiles/broccoli.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/broccoli.dir/broccoli/log.cpp.o"
+  "CMakeFiles/broccoli.dir/broccoli/util.cpp.o"
+  "libbroccoli.pdb"
+  "libbroccoli.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/broccoli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tests/test_config.cpp
@@ -0,0 +1,8 @@
+#include "../broccoli/config.h"
+#include "../broccoli/log.h"
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
