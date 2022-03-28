35020dc1e220b7673b0ddf165b75da2e8a93da44
cheng <lambert2478@163.com>
2022-03-28 22:11:21 +0800

    config module vector arr 特化

--- broccoli/config.h
+++ broccoli/config.h
@@ -31,11 +31,50 @@ namespace broccoli {
 		std::string m_description;
 	};
 
+	template<class F, class T>
+	class LexicalCast {
+	public:
+		T operator()(const F& v) {
+			return boost::lexical_cast<T>(v);
+		}
+	};
+
+	template<class T> 
+	class LexicalCast<std::string, std::vector<T>> {
+	public:
+		std::vector<T> operator() (const std::string& v) {
+			YAML::Node node = YAML::Load(v);
+			typename std::vector<T> vec;
+			std::stringstream ss;
+			for(size_t i = 0; i<node.size(); ++i) {
+				ss.str("");
+				ss << node[i];
+				vec.push_back(LexicalCast<std::string, T>()(ss.str()));
+			}
+			return vec;
+		}
+	};
+
+	template<class T>
+	class LexicalCast<std::vector<T>, std::string> {
+	public:
+		std::string operator() (const std::vector<T>& v) {
+			YAML::Node node;
+			for(auto& i : v) {
+				node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+			}
+			std::stringstream ss;
+			ss << node;
+			return ss.str();
+		}
+	};
+
 	/*
 	*	继承于 ConfigVarBase
 	*	配置信息存储类型，每条配置信息格式由此class定义
 	**/
-	template<class T>
+	template<class T, class FormStr = LexicalCast<std::string, T>
+				, class ToStr = LexicalCast<T, std::string>>
 	class ConfigVar : public ConfigVarBase {
 	public:
 		typedef std::shared_ptr<ConfigVar> ptr;
@@ -49,7 +88,8 @@ namespace broccoli {
 
 		std::string toString() override {
 			try {
-				return boost::lexical_cast<std::string>(m_val);
+				// return boost::lexical_cast<std::string>(m_val);
+				return ToStr()(m_val);
 			} 
 			catch (const std::exception& e) {
 				BROCCOLI_LOG_ERROR(BROCCOLI_LOG_ROOT()) << "ConfigVar::toString exception"
@@ -60,7 +100,8 @@ namespace broccoli {
 
 		bool fromString(const std::string& val) override {
 			try {
-				m_val = boost::lexical_cast<T>(val);
+				// m_val = boost::lexical_cast<T>(val);
+				setValue(FormStr()(val));
 				return true;
 			}
 			catch (const std::exception& e) {
--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -5,6 +5,12 @@
 broccoli::ConfigVar<int>::ptr g_int_value_config = 
         broccoli::Config::Lookup("system.port", (int)8080, "system port");
 
+broccoli::ConfigVar<float>::ptr g_float_value_config = 
+        broccoli::Config::Lookup("system.value", (float)10.2f, "system value");
+
+broccoli::ConfigVar<std::vector<int>>::ptr g_int_vec_value_config = 
+        broccoli::Config::Lookup("system.int_vec", std::vector<int>{1, 2}, "system int vec");
+
 void print_yaml(const YAML::Node& node, int level){
     if(node.IsScalar()){
         BROCCOLI_LOG_INFO(BROCCOLI_LOG_ROOT()) << std::string(level*4, ' ')
@@ -34,12 +40,32 @@ void test_yaml(){
     print_yaml(root, 0);
 }
 
+void test_config(){
+    BROCCOLI_LOG_INFO(BROCCOLI_LOG_ROOT()) << "before: " << g_int_value_config->getValue();
+    BROCCOLI_LOG_INFO(BROCCOLI_LOG_ROOT()) << "before: " << g_float_value_config->toString();
+
+    auto v = g_int_vec_value_config->getValue();
+    for(auto& i : v) {
+        BROCCOLI_LOG_INFO(BROCCOLI_LOG_ROOT()) << "before int_vec: " << i;
+    }
+
+    YAML::Node root = YAML::LoadFile("/home/cheng/workspace/broccoli/bin/conf/log.yaml");
+    broccoli::Config::LoadFromYaml(root);
+
+    BROCCOLI_LOG_INFO(BROCCOLI_LOG_ROOT()) << "after: " << g_int_value_config->getValue();
+    BROCCOLI_LOG_INFO(BROCCOLI_LOG_ROOT()) << "after: " << g_float_value_config->toString();
+
+    v = g_int_vec_value_config->getValue();
+    for(auto& i : v) {
+        BROCCOLI_LOG_INFO(BROCCOLI_LOG_ROOT()) << "after int_vec: " << i;
+    }
+}
+
 int main(int argc, char** argv)
 {
-    BROCCOLI_LOG_INFO(BROCCOLI_LOG_ROOT()) << g_int_value_config->getValue();
-    BROCCOLI_LOG_INFO(BROCCOLI_LOG_ROOT()) << g_int_value_config->toString();
+    // test_yaml();
 
-    test_yaml();
+    test_config();
 
     return 0;
 }
